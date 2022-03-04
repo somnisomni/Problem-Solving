aff1fe82d631b75bcce6e0bbe5129a74e1103be7
somni <me@somni.one>
2022-03-04 22:31:55 +0900

    Comment

--- Programmers/SQL_HighScore_Kit/5_JOIN/4_Animals_Neutered_at_Shelter.sql
+++ Programmers/SQL_HighScore_Kit/5_JOIN/4_Animals_Neutered_at_Shelter.sql
@@ -8,3 +8,6 @@ SELECT ANIMAL_INS.ANIMAL_ID, ANIMAL_INS.ANIMAL_TYPE, ANIMAL_INS.NAME
   RIGHT OUTER JOIN ANIMAL_OUTS ON ANIMAL_INS.ANIMAL_ID = ANIMAL_OUTS.ANIMAL_ID
   WHERE ANIMAL_INS.SEX_UPON_INTAKE != ANIMAL_OUTS.SEX_UPON_OUTCOME
   ORDER BY ANIMAL_OUTS.ANIMAL_ID
+-- 중성화 여부는 SEX_UPON_INCOME -> SEX_UPON_OUTCOME 비교 시 값 자체가 달라지는 경우 중성화라고 판단합니다.
+--   자연(Intact) 상태에서 중성화(Neutered/Spayed) 상태로 바뀔 수는 있어도, 중성화 상태에서 자연 상태로 바뀌는 경우는 없다고 가정하고,
+--   성별이 바뀌는 경우 또한 없다고 가정한다면, 단순히 값 비교만으로 중성화 판단이 가능합니다.
