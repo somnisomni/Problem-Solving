24272cbacfe7df1d41ae166557f7cc0959860abe
somni <me@somni.one>
2023-05-16 15:27:53 +0900

    Add BOJ/10813

---
+++ BOJ/10813.py
@@ -0,0 +1,8 @@
+N, M = map(int, input().split())
+arr = [str(n) for n in range(1, N + 1)]
+
+for _ in range(M):
+  i, j = map(int, input().split())
+  arr[i - 1], arr[j - 1] = arr[j - 1], arr[i - 1]
+
+print(" ".join(arr))
